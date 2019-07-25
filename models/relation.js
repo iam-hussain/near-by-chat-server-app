a8676a68096d2e5c1ce7103339bbdb25aa87e46a
Zakir Hussain <44015638+ZaHuPro@users.noreply.github.com>
2019-07-25 22:53:38 +0530

    GraphQl

---
+++ models/relation.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+const timestamps = require('mongoose-timestamp');
+
+var RelationSchema = new mongoose.Schema({
+    relating: {
+        require: true,
+        type: Schema.Types.ObjectId, ref: 'User'
+    },
+    related: {
+        require: true,
+        type: Schema.Types.ObjectId, ref: 'User'
+    },
+    status: {
+        require: true,
+        type: String,
+    }
+});
+
+mongoose.plugin(timestamps, {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+});
+
+
+module.exports = mongoose.model("Relation", RelationSchema);
