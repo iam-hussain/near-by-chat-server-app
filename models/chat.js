0f16f739557a89c31fdf76eedca58bdc957b4181
Zakir Hussain <44015638+ZaHuPro@users.noreply.github.com>
2019-07-27 12:31:16 +0530

    Chat Schema

---
+++ models/chat.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+const timestamps = require('mongoose-timestamp');
+const Schema = mongoose.Schema;
+
+
+var conversation = new mongoose.Schema({
+    author: {
+        require: true,
+        type: Schema.Types.ObjectId, ref: 'User'
+    },
+    body: String,
+    attachment: String,
+    created: {type: Date, default:Date.now}
+});
+
+var ChatSchema = new mongoose.Schema({
+    members: [{ type : Schema.Types.ObjectId, ref: 'User' }],
+    messages: [conversation]
+});
+
+// mongoose.plugin(timestamps, {
+//     createdAt: 'created_at',
+//     updatedAt: 'updated_at'
+// });
+
+
+module.exports = mongoose.model("Chat", ChatSchema);
